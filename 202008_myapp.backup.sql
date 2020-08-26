4dbe0b1622daa6746a093085857eab820eaea18a
motoyu5623 <motoyu5623@gmail.com>
2020-08-27 08:56:38 +0900

    add backup file

---
+++ 202008_myapp.backup.sql
@@ -0,0 +1,129 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for osx10.15 (x86_64)
+--
+-- Host: localhost    Database: myapp
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` int unsigned NOT NULL,
+  `body` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,1,'first comment for post 1'),(2,1,'second comment for post 1'),(3,3,'first comment for post 3'),(4,4,'first comment for post 4'),(5,4,'first comment for new post');
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logs` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `msg` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logs`
+--
+
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `body` text,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'title 1','body 1','2020-08-26 15:38:22','2020-08-26 15:38:22'),(2,'updated','body 2','2020-08-26 15:38:22','2020-08-26 15:40:23'),(3,'title 3','body 3','2020-08-26 15:38:22','2020-08-26 15:38:22');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `score` float DEFAULT NULL,
+  `coins` set('gold','silver','bronze') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'taguchi',4.6,'silver'),(2,'fkoji',9.4,'gold'),(3,'dotinstall',6.1,'gold,silver'),(4,'yamada',2.1,'bronze');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-26 15:43:48
--- blog.sql
+++ blog.sql
@@ -2,15 +2,26 @@ drop table if exists posts;
 create table posts (
   id int unsigned primary key auto_increment,
   title varchar(255),
-  body text
+  body text,
+  created datetime default current_timestamp,
+  updated datetime default current_timestamp on update current_timestamp
 );
 
-drop table if exists comments;
-create table comments (
-  id int unsigned primary key auto_increment,
-  post_id int unsigned not null,
-  body text
-);
+-- drop table if exists logs;
+-- create table logs (
+--   id int unsigned primary key auto_increment,
+--   msg varchar(255)
+-- );
+
+-- drop trigger if exists posts_insert_trigger;
+-- create trigger posts_insert_trigger after insert on posts for each row insert into logs (msg) values ('post added!');
+
+-- drop table if exists comments;
+-- create table comments (
+--   id int unsigned primary key auto_increment,
+--   post_id int unsigned not null,
+--   body text
+-- );
 
 -- alter table comments add constraint fk_comments foreign key(post_id) references posts (id);
 
@@ -18,17 +29,20 @@ insert into posts (title, body) values ('title 1', 'body 1');
 insert into posts (title, body) values ('title 2', 'body 2');
 insert into posts (title, body) values ('title 3', 'body 3');
 
-insert into comments (post_id, body) values (1, 'first comment for post 1');
-insert into comments (post_id, body) values (1, 'second comment for post 1');
-insert into comments (post_id, body) values (3, 'first comment for post 3');
-insert into comments (post_id, body) values (4, 'first comment for post 4');
-
-delete from posts where id = 2;
-insert into posts (title, body) values ('new title', 'new body');
-insert into comments (post_id, body) values (last_insert_id(), 'first comment for new post');
-
 select * from posts;
-select * from comments;
+-- select * from logs;
+
+-- insert into comments (post_id, body) values (1, 'first comment for post 1');
+-- insert into comments (post_id, body) values (1, 'second comment for post 1');
+-- insert into comments (post_id, body) values (3, 'first comment for post 3');
+-- insert into comments (post_id, body) values (4, 'first comment for post 4');
+
+-- delete from posts where id = 2;
+-- insert into posts (title, body) values ('new title', 'new body');
+-- insert into comments (post_id, body) values (last_insert_id(), 'first comment for new post');
+
+-- select * from posts;
+-- select * from comments;
 
 -- select * from posts inner join comments on posts.id = comments.post_id;
 -- select posts.id, posts.title, posts.body, comments.body from posts inner join comments on posts.id = comments.post_id;
